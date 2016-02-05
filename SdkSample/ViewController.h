60c7663badf6435c9b6e7880de4c7582cf8f3f36
Roman Slysh <monar.ua@gmail.com>
2016-02-05 13:13:23 +0200

    Initial Commit

---
+++ SdkSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SdkSample
+//
+//  Created by Roman Slysh on 2/5/16.
+//  Copyright © 2016 Roman Slysh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
